ef24ab27c10eaf65f13da615e76f0be557a1e5ad
Maruf131 <mdsheikhmaruf131@gmail.com>
2026-05-22 23:34:44 +0600

    authorize middleware create for role base access control

--- src/modules/middleware/authorize.ts
+++ src/modules/middleware/authorize.ts
@@ -1,13 +1,12 @@
 export const authorize = (...roles: string[]) => {
   return (req: any, res: any, next: any) => {
-    // role check
+    // Check the role
     if (!roles.includes(req.user.role)) {
       return res.status(403).json({
         success: false,
         message: "Forbidden Access",
       });
     }
-
     next();
   };
 };
